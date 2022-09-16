1aac429b3bd398fc629dd542d2d7fa0c7c707504
OliverMax <nekrasov@yelk.com.ua>
2022-09-17 02:20:48 +0300

    split code into functions

--- src/utils/dom/scroll.ts
+++ src/utils/dom/scroll.ts
@@ -17,7 +17,7 @@ type ScrollDirectionY = 'top' | 'bottom';
 export default function scroll(
   target: HTMLLIElement,
   settings?: Settings,
-): Promise<void> | undefined | void {
+): Promise<void> | void {
   const {
     type,
     alignY,
@@ -64,55 +64,19 @@ export default function scroll(
     scrollHeight,
   } = target.parentElement!;
 
-  const scrollSpaceLeft = targetX - containerX + scrollOffsetX;
-  const scrollSpaceRight = scrollWidth - (scrollSpaceLeft + targetWidth);
-  const scrollSpaceTop = targetY - containerY + scrollOffsetY;
-  const scrollSpaceBottom = scrollHeight - (scrollSpaceTop + targetHeight);
-
-  // corrections depends on align
-  let alignCorrectionX = 0,
-      alignCorrectionY = 0;
-
-  if (alignX === 'center') {
-    alignCorrectionX = (containerWidth / 2) - (targetWidth / 2);
-  } else if (alignX === 'right') {
-    alignCorrectionX = containerWidth - targetWidth;
-  }
-
-  if (alignY === 'center') {
-    alignCorrectionY = (containerHeight / 2) - (targetHeight / 2);
-  } else if (alignY === 'bottom') {
-    alignCorrectionY = containerHeight - targetHeight;
-  }
-
-  // round corrections. because can't be 0.5px
-  alignCorrectionX = Math.round(alignCorrectionX);
-  alignCorrectionY = Math.round(alignCorrectionY);
+  const alignCorrection = getAlignCorrection();
 
   // apply align corrections
-  distanceX -= alignCorrectionX;
-  distanceY -= alignCorrectionY;
+  distanceX -= alignCorrection.x;
+  distanceY -= alignCorrection.y;
 
-  // define scroll direction
-  let SCROLL_DIRECTION_X: null | ScrollDirectionX = null;
-  let SCROLL_DIRECTION_Y: null | ScrollDirectionY = null;
-  
-  if (distanceX > 0) {
-    SCROLL_DIRECTION_X = 'right';
-  } else if (distanceX < 0) {
-    SCROLL_DIRECTION_X = 'left';
-  }
-
-  if (distanceY < 0) {
-    SCROLL_DIRECTION_Y = 'top';
-  } else if (distanceY > 0) {
-    SCROLL_DIRECTION_Y = 'bottom';
-  }
+  const scrollDirection = getScrollDirection();
+  const scrollSpace = getScrollSpace();
 
   // !FIXME: сделать поправку на оставшееся место после блока
   // correct distance depends on available scroll space
 
-  if (SCROLL_DIRECTION_X || SCROLL_DIRECTION_Y) {
+  if (scrollDirection.x || scrollDirection.y) {
     const ANIMATION_STEP = 8;
 
     let animationId: null | number = null;
@@ -156,4 +120,64 @@ export default function scroll(
       }
     }
   }
+
+  // fn's
+  function getAlignCorrection() {
+    let alignCorrectionX = 0,
+        alignCorrectionY = 0;
+
+    if (alignX === 'center') {
+      alignCorrectionX = (containerWidth / 2) - (targetWidth / 2);
+    } else if (alignX === 'right') {
+      alignCorrectionX = containerWidth - targetWidth;
+    }
+
+    if (alignY === 'center') {
+      alignCorrectionY = (containerHeight / 2) - (targetHeight / 2);
+    } else if (alignY === 'bottom') {
+      alignCorrectionY = containerHeight - targetHeight;
+    }
+
+    // round corrections. because can't be 0.5px
+    return {
+      x: Math.round(alignCorrectionX),
+      y: Math.round(alignCorrectionY),
+    };
+  }
+
+  function getScrollDirection() {
+    let x: null | ScrollDirectionX = null;
+    let y: null | ScrollDirectionY = null;
+    
+    if (distanceX > 0) {
+      x = 'right';
+    } else if (distanceX < 0) {
+      x = 'left';
+    }
+  
+    if (distanceY < 0) {
+      y = 'top';
+    } else if (distanceY > 0) {
+      y = 'bottom';
+    }
+
+    return {
+      x: x,
+      y: y,
+    };
+  }
+
+  function getScrollSpace() {
+    const scrollSpaceLeft = targetX - containerX + scrollOffsetX;
+    const scrollSpaceRight = scrollWidth - (scrollSpaceLeft + targetWidth);
+    const scrollSpaceTop = targetY - containerY + scrollOffsetY;
+    const scrollSpaceBottom = scrollHeight - (scrollSpaceTop + targetHeight);
+
+    return {
+      left: scrollSpaceLeft,
+      top: scrollSpaceTop,
+      right: scrollSpaceRight,
+      bottom: scrollSpaceBottom,
+    };
+  };
 };
